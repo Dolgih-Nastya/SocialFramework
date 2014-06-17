03e6c238c64d495525a6b19ace393d92ed759120
Анастасия Долгих <anastasiadolgih@Anastasias-Mac.local>
2014-06-17 07:43:21 -0700

    Initial Commit

---
+++ SocialFrameworkTest/SFTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFTAppDelegate.h
+//  SocialFrameworkTest
+//
+//  Created by Анастасия Долгих on 6/17/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialFrameworkTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocialFrameworkTest
+//
+//  Created by Анастасия Долгих on 6/17/14.
+//  Copyright (c) 2014 Anastasia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFTAppDelegate class]));
+    }
+}
